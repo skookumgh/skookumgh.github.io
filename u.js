856666c21e7a8876e4a29c3ddd89c392ce87bf6a
skookumgh <64236425+skookumgh@users.noreply.github.com>
2020-04-25 10:03:49 +0800

    Update u.js

--- u.js
+++ u.js
@@ -26,9 +26,9 @@ class NitroBlock {
             }],
         "menus": { //we will get back to this in a later tutorial
         }
-    };
+    }
     substringy({num1, num2, string}) {
         return string.substring(num1 - 1, num2);
-    };
+    }
 }
 Scratch.extensions.register(new NitroBlock())
